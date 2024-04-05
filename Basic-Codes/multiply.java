123865362a000a9965970dfa2c7bd03f97baab84
SageSachin <sachinkvns9@gmail.com>
2024-04-05 14:17:30 +0530

    added multiply code

---
+++ Basic-Codes/multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=89;
+        int b=70;
+        int product=a*b;
+        System.out.println("Product is: "+product);
+    }
+}
