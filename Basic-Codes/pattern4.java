9eb14af579311ba9d1abf5e880689103cfe7d625
SageSachin <sachinkvns9@gmail.com>
2024-04-06 12:38:14 +0530

    added comments

--- Basic-Codes/pattern4.java
+++ Basic-Codes/pattern4.java
@@ -1,9 +1,14 @@
+// A 
+// B C
+// D E F
+// G H I J
+
 public class pattern4 {
     public static void main(String[] args) {
         char ch=65;
         for(int i=1;i<=4;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(ch);
+                System.out.print(ch + " ");
                 ch++;
             }
             System.out.println();
