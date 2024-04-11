6038446c2533ca24ee2020148f4f39738e6f4e2a
SageSachin <sachinkvns9@gmail.com>
2024-04-11 23:25:14 +0530

    added new array code

---
+++ Java-Arrays/array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class array{
+    public static void main(String []args){
+        int arr[]=new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Values: ");
+        for(int i=0;i<5;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(arr);
+        System.out.println();
+        for(int i=0;i<5;i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
