fdbcd83c1a44f122f8f5fb0853affa44217327fc
SageSachin <sachinkvns9@gmail.com>
2024-04-12 22:57:43 +0530

    added linear search code

---
+++ Java-Arrays/linear_search.java
@@ -0,0 +1,15 @@
+public class linear_search {
+    public static int linear_searcher(int key, int x[]){
+        for(int i=0;i<x.length;i++){
+            if(x[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int x[]={2,3,5,6,8,9,1,4};
+        int key=9;
+        System.out.println(key+" is present on index "+linear_searcher(key,x));
+    }
+}
