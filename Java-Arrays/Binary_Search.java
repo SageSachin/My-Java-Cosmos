8f6db0ebf6c612daabecf9ac23b810f817e43c15
SageSachin <sachinkvns9@gmail.com>
2024-04-14 15:47:53 +0530

    added new code

--- Java-Arrays/Binary_Search.java
+++ Java-Arrays/Binary_Search.java
@@ -2,15 +2,15 @@ public class Binary_Search {
     public static void main(String[] args) {
         int arr[]={2,3,4,5,6,7,8,9}; //sorted array
         int start=0, end=arr.length-1;
-        int key=7;
+        int key=4;
         while(start<=end){
             int mid=(start+end)/2;
 
             if(key==arr[mid]){
-                System.out.println("Starting from zero, "+key+" is on index: "+mid);
+                System.out.println("Starting from zero, "+key+" is on index "+mid);
             }
             
-            if(key>arr[mid]){
+            if(key>arr[mid]){  
                 start=mid+1;
             }
             else{
