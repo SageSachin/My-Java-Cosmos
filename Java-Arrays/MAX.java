03a1933e31753dd796e11e56def89b2c791df0f6
SageSachin <sachinkvns9@gmail.com>
2024-04-14 15:39:12 +0530

    added new code

---
+++ Java-Arrays/MAX.java
@@ -0,0 +1,12 @@
+public class MAX {
+    public static void main(String[] args) {
+        int smallest = Integer.MAX_VALUE;
+        int arr[]={2,4,3,1,5,10,6};
+        for(int i=0;i<arr.length;i++){
+            if(smallest>arr[i]){
+                smallest=arr[i];
+            }
+        }
+        System.out.println("Smallest number in the array is: "+smallest);
+    }
+}
